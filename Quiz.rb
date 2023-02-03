9416c7c60cdb0424aa1d1f26e0a9c56f8040b8a8
BlueKNIGHT107 <aayushd99@gmail.com>
2023-02-03 19:57:05 +0530

    Basics of Ruby.

---
+++ Quiz.rb
@@ -0,0 +1,6 @@
+address = [1,2,3,4,5,6,7,8,9]
+p "Address: #{address}"
+p "Address[5]: #{address[5]}"
+
+new_address = address.reverse!
+p "New address: #{new_address}"
---
+++ print_puts_p.rb
@@ -0,0 +1,4 @@
+print "Output of print."
+puts "Output of puts."
+p "Output of p."
+puts "Output of puts."
