00d7be04174e436f3641a3352621abafd40a9144
BlueKNIGHT107 <aayushd99@gmail.com>
2023-02-21 16:50:23 +0530

    Articles completed

---
+++ Rails/ROR_Assignment/app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description, :author))
+        if @article.save
+            flash[:notice] = "Article created successfully."
+            redirect_to @article
+        else
+            puts "======================"
+            render "new", status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description, :author))
+            flash[:notice] = "Article updated successfully."
+            redirect_to @article
+        else
+            render "edit", status: :unprocessable_entity
+        end
+    end
+end
